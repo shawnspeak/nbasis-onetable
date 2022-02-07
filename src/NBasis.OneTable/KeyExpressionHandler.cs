1499e52ec11c557cc960947dd1063f61ea8ce1b2
shawnspeak <shawnb@xcarab.com>
2022-02-07 14:40:04 -0800

    Working on key expressions

--- src/NBasis.OneTable/KeyExpressionHandler.cs
+++ src/NBasis.OneTable/KeyExpressionHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Amazon.DynamoDBv2.Model;
 using NBasis.OneTable.Annotations;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace NBasis.OneTable
 {
@@ -10,8 +11,8 @@ namespace NBasis.OneTable
      * - Most key expressions should be PK + SK
      * - Possible that PK has SK attribute and get's added twice
      * - PK Must always be specified
+     * - If TItem has an SK, then SK must always be specified
      */
-
 
     public class KeyItemExpressionHandler<TItem> where TItem : class
     {
@@ -20,6 +21,55 @@ namespace NBasis.OneTable
         public KeyItemExpressionHandler(TableContext context)
         {
             _context = context;
+        }
+
+        private void FindKeys(BinaryExpression operation, FoundKeys foundKeys)
+        {
+            var left = operation.Left;
+            var right = operation.Right;
+            
+            if ((left != null) && (left is BinaryExpression leftBi))
+                FindKeys(leftBi, foundKeys);
+            else if ((right != null) && (right is BinaryExpression rightBi))
+                FindKeys(rightBi, foundKeys);
+            else if ((operation.NodeType == ExpressionType.Equal) &&
+                     (left != null) && 
+                     (right != null))
+            {
+                if (left.NodeType == ExpressionType.MemberAccess)
+                {
+                    if (left is System.Linq.Expressions.MemberExpression leftNode)
+                    {
+                        // must contain a key attribute
+                        if (leftNode.Member.CustomAttributes.Any(ca => ca.AttributeType == typeof(PKAttribute)))
+                        {
+                            // then right must contain a value
+                            if (right.NodeType == ExpressionType.Constant)
+                            {
+                                var rightNode = right as System.Linq.Expressions.ConstantExpression;
+                                if (rightNode != null)
+                                {
+                                    if (rightNode.Value is string)
+                                    {
+                                        // keyItem[_context.Configuration.KeyAttributes.PKName] = new AttributeValue(rightNode.Value.ToString());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public class FoundKeys
+        {
+            public MemberInfo PKMember { get; set; }
+
+            public AttributeValue PKValue { get; set; }
+
+            public MemberInfo SKMember { get; set; }
+
+            public AttributeValue SKValue { get; set; }
         }
 
         public Dictionary<string, Amazon.DynamoDBv2.Model.AttributeValue> Handle(Expression<Func<TItem, bool>> predicate)
@@ -35,33 +85,38 @@ namespace NBasis.OneTable
             if (operation == null)
                 throw new ArgumentException("Must be a binary expression");
 
-            var left = operation.Left;
-            var right = operation.Right;
-            
-            if (left.NodeType == ExpressionType.MemberAccess)
-            {
-                var leftNode = left as System.Linq.Expressions.MemberExpression;
-                if (leftNode != null)
-                {
-                    // must contain a key attribute
-                    if (leftNode.Member.CustomAttributes.Any(ca => ca.AttributeType == typeof(PKAttribute)))
-                    {
-                        // then right must contain a value
 
-                        if (right.NodeType == ExpressionType.Constant)
-                        {
-                            var rightNode = right as System.Linq.Expressions.ConstantExpression;
-                            if (rightNode != null)
-                            {
-                                if (rightNode.Value is string)
-                                {
-                                    keyItem[_context.Configuration.KeyAttributes.PKName] = new AttributeValue(rightNode.Value.ToString());
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            // recurse expression tree and look for PK and SK
+            var foundKeys = new FoundKeys();
+            FindKeys(operation as BinaryExpression, foundKeys);
+
+            //var left = operation.Left;
+            //var right = operation.Right;
+            
+            //if (left.NodeType == ExpressionType.MemberAccess)
+            //{
+            //    var leftNode = left as System.Linq.Expressions.MemberExpression;
+            //    if (leftNode != null)
+            //    {
+            //        // must contain a key attribute
+            //        if (leftNode.Member.CustomAttributes.Any(ca => ca.AttributeType == typeof(PKAttribute)))
+            //        {
+            //            // then right must contain a value
+
+            //            if (right.NodeType == ExpressionType.Constant)
+            //            {
+            //                var rightNode = right as System.Linq.Expressions.ConstantExpression;
+            //                if (rightNode != null)
+            //                {
+            //                    if (rightNode.Value is string)
+            //                    {
+            //                        keyItem[_context.Configuration.KeyAttributes.PKName] = new AttributeValue(rightNode.Value.ToString());
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
 
             return keyItem;
         }
